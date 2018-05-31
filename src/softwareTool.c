d10593d986c0b5241f37beee64c45c864b5a249e
unknown <zfang123@icloud.com>
2018-05-31 17:29:39 +0800

    update

--- src/softwareTool.c
+++ src/softwareTool.c
@@ -4,16 +4,16 @@
 void selectionSort(int arr[], int n) {
 	int i, j;
 	for (i = 0; i < n - 1; i++) {
-		int min = i;
+		int minIndex = i;
 		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[min]) {
-				min = j;
+			if (arr[minIndex] > arr[j]) {
+				minIndex = j;
 			}
 		}
 
 		int tem = arr[i];
-		arr[i] = arr[min];
-		arr[min] = tem;
+		arr[i] = arr[minIndex];
+		arr[minIndex] = tem;
 	}
 }
 
