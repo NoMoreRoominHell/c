4e845d15133873cad01ce670f415e578788bdcca
unknown <zfang123@icloud.com>
2018-05-31 13:35:37 +0800

    selection sort

--- src/softwareTool.c
+++ src/softwareTool.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+void selectionSort(int arr[], int n) {
+	int i, j;
+	for (i = 0; i < n - 1; i++) {
+		int min = i;
+		for (j = i + 1; j < n; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		if (i != min) {
+			int tem = arr[i];
+			arr[i] = arr[min];
+			arr[min] = tem;
+		}
+	}
+}
 
+void arrPrintf(int arr[], int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
+int main(void) {
+	int arr[] = { 9, 8, 5, 17, 1, 2, 3 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	selectionSort(arr, size);
+	arrPrintf(arr, size);
 	return EXIT_SUCCESS;
 }
