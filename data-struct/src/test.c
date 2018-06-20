4c6144b231f8e8626f6e2d49d1c3d9bdc78c5793
unknown <zfang123@icloud.com>
2018-06-20 13:33:43 +0800

    new file

--- data-struct/src/test.c
+++ data-struct/src/test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-	printf("test");
+//main_test
+int main_test() {
+	int a;
+	for (int i = 1; i <= 6; i++) {
+		scanf("%d", &a);
+		printf("%d", a);
+	}
 	return 0;
 }
