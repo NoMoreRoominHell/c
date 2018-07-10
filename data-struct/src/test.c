cbbb6bb138ab8a67375f20e7eaee9456a6b4fdf7
NoMoreRoominHell <zfang123@icloud.com>
2018-07-10 22:31:11 +0800

    new

--- data-struct/src/test.c
+++ data-struct/src/test.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX(x,y) ((x)>(y)?(x):(y))
-#define IS_EVEN(n) ((n)%2==0)
+#define FILE_NAME "example.dat"
 
-//检测字符c是否在'a'与'z'之间，在：返回对应的大写，不在：原值返回
-#define TOUPPER(c) ('a'<=(c)&&(c)<='z'?(c)-'a'+'A':(c))
-
-#define PRINT_INT(n) printf(#n " = %d\n",n)
-
-#define MK_ID(n) i##n
-
-int fact(int n) {
-	if (n <= 1)
-		return 1;
-	else
-		return n * fact(n - 1);
-}
-
-int main() {
-	char *s = "hello";
+int main(void) {
+	FILE *fp = fopen(FILE_NAME, "r");
+	if (fp == NULL) {
+		printf("Can't open %s\n", FILE_NAME);
+		exit(EXIT_FAILURE);
+	}
+	//...
+	fclose(fp);
 	return 0;
 }
